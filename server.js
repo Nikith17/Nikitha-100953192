2d0b1b15dbb93a733fa74cbe337b5c343aaa1941
Nikith17 <nikithareddy1799@gmail.com>
2025-02-17 03:03:43 -0500

    Add server.js to handle application startup

--- server.js
+++ server.js
@@ -1,17 +1,16 @@
 const express = require('express');
 const app = express();
 
-// Other middlewares and routes
+// Middleware
 app.use(express.json());
 
 // Example route
 app.get('/', (req, res) => {
-  // *Successfully my application is running by visiting the URL provided by Cloud Run*
   res.send('Successfully my application is running by visiting the URL provided by Cloud Run');
 });
 
 // Listen on the environment's PORT or default to 3000
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(Server running on port ${port});
+  console.log(`Server running on port ${port}`);
 });
